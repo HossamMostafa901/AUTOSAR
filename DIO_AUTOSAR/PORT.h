f584edca8743ff49a8d9ea43c8407b8368d71e13
HossamMostafa901 <142822458+HossamMostafa901@users.noreply.github.com>
2023-10-07 22:53:55 +0300

    Add files via upload

---
+++ DIO_AUTOSAR/PORT.h
@@ -0,0 +1,17 @@
+/*
+ * PORT.h
+ *
+ *  Created on: Oct 7, 2023
+ *      Author: DELL
+ */
+
+#ifndef PORT_H_
+#define PORT_H_
+
+#include"tm4c123gh6pm.h"
+#include"std_types.h"
+#include "bitwise_operation.h"
+
+void PORT_INIT(void);
+
+#endif /* PORT_H_ */
